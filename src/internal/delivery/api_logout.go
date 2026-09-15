126e10d6359f71f31d738d80aea02d9769df45f8
asli0171 <aslifarrah@gmail.com>
2026-09-15 12:03:07 +0200

    logout - session cookies slettet og redirected til login

---
+++ src/internal/delivery/api_logout.go
@@ -0,0 +1,22 @@
+package delivery
+
+import (
+	"database/sql"
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"github.com/visionJAMx/whoknows/src/service"
+)
+
+func LogoutHandler(c *gin.Context) {
+session := sessions.Default(c)
+    session.Clear()
+    session.Save()
+
+	c.SetCookie("auth_token", "", -1, "/", "localhost", false, true)
+
+	c.Redirect(http.StatusSeeOther, "/login")
+}
